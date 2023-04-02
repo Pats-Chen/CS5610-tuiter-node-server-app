a649a4fa35c58f4c8de8ad2219b4e926f85438b8
Jiazhe Chen <chenjiazhe@outlook.com>
2023-04-02 17:16:47 -0400

    Change tuit posts

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -1,23 +1,40 @@
 export default [
     {
-        "_id": "123", "topic": "Space", "username": "SpaceX",
-        "handle": "@spacex", "time": "2h", "image": "spacex.png",
-        "title": "SpaceX's Mission",
-        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+        "_id": "1",
+        "userIcon": "fox.png",
+        "userName": "Alice",
+        "handle": "@alice",
+        "tuit": "Amazing film by Christopher Nolan!",
+        "topic": "Movie",
+        "time": "2h",
+        "image": "inception.jpg",
+        "title": "Inception | Movies - WarnerBros.com",
+        "content": "Your mind is the scene of the crime. Box-office superstar Leonardo DiCaprio stars in this contemporary science fiction action film set \"within the architecture of the mind.\"",
+        "href": "https://www.warnerbros.com/movies/inception",
+        "replies": 4200,
+        "retuits": 3500,
+        "likes": 743,
+        "liked": true,
+        "dislikes": 347,
+        "disliked": false
     },
     {
-        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
-        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
-        "title": "The Boring Company fixing traffic",
-        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
-        "liked": true, "likes": 2345, "replies": 321, "retuits": 432
-    },
-    {
-        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
-        "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
-        "title": "Neurolink fixes paralisis and blindness",
-        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
+        "_id": "2",
+        "userIcon": "cow.webp",
+        "userName": "Jiazhe",
+        "handle": "@jiazhe",
+        "tuit": "Amazing books written by Murakami Haruki!",
+        "topic": "Book",
+        "time": "23h",
+        "image": "murakami-books.jpg",
+        "title": "",
+        "content": "",
+        "href": "https://www.harukimurakami.com/",
+        "replies": 965,
+        "retuits": 2400,
+        "likes": 652,
+        "liked": false,
+        "dislikes": 256,
+        "disliked": false
     },
 ];
